83db3cbb64a91c4f63b5cc03602daea7b452afcb
bhasker1996 <bhasker.medathati@gmail.com>
2024-04-15 18:46:25 +0530

    Added Car Class

---
+++ src/Car.java
@@ -0,0 +1,21 @@
+public class Car
+{
+    private int price;
+    private int engineSize;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getEngineSize() {
+        return engineSize;
+    }
+
+    public void setEngineSize(int engineSize) {
+        this.engineSize = engineSize;
+    }
+}
